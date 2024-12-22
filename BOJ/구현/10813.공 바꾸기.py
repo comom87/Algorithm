b773699ec32d0084e7e3d9b3e8258775765c2a03
comom87 <dmsrkql104321@gmail.com>
2024-12-23 00:01:44 +0900

    백준: 10813.공 바꾸기

---
+++ BOJ/구현/10813.공 바꾸기.py
@@ -0,0 +1,6 @@
+n, m = map(int, input().split())
+baskets = [i for i in range(1, n + 1)]
+for _ in range(m):
+    i, j = map(int, input().split())
+    baskets[i - 1], baskets[j - 1] = baskets[j - 1], baskets[i - 1]
+print(*baskets)
