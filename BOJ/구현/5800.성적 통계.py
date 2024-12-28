552df5d2850029c28578b6ac40df60b75cf0e88a
comom87 <dmsrkql104321@gmail.com>
2024-12-29 00:36:28 +0900

    백준: 5800.성적 통계

--- BOJ/구현/5800.성적 통계.py
+++ BOJ/구현/5800.성적 통계.py
@@ -7,4 +7,4 @@ for i in range(k):
     math_scores.sort(reverse=True)
     gaps = [math_scores[i] - math_scores[i + 1] for i in range(n - 1)]
     print(f'Class {i + 1}')
-    print(f'Max {math_scores[0]} Min {math_scores[-1]} Largest gap {max(gaps)}')
+    print(f'Max {math_scores[0]}, Min {math_scores[-1]}, Largest gap {max(gaps)}')
