551365f979bdf2714ef695f535e492837612e350
comom87 <dmsrkql104321@gmail.com>
2025-01-13 02:00:17 +0900

    백준: 1018.체스판 다시 칠하기

--- BOJ/구현/1018.체스판 다시 칠하기.py
+++ BOJ/구현/1018.체스판 다시 칠하기.py
@@ -6,41 +6,25 @@ board = [list(input()) for _ in range(n)]
 minCnt = 1e9
 for i in range(n - 7):
     for j in range(m - 7):
-        # 검은색으로 변경해야 하는 칸의 수
-        paintBCnt = 0
-        # 흰색으로 변경해야 하는 칸의 수
-        paintWCnt = 0
-        for y in range(i, i + 8):
-            for x in range(j, j + 9):
-                if (i + j) % 2 == 0:
-                    if (x + y) % 2 == 0:
-                        # board[i][j] = 'B'인 경우 = 검은색으로 시작하는 경우
-                        if board[y][x] != 'B':
-                            paintBCnt += 1
-                        # board[i][j] = 'W'인 경우 = 흰색으로 시작하는 경우
-                        if board[y][x] != 'W':
-                            paintWCnt += 1
-                    else:
-                        # board[i][j] = 'B'인 경우 = 검은색으로 시작하는 경우
-                        if board[y][x] != 'W':
-                            paintWCnt += 1
-                        # board[i][j] = 'W'인 경우 = 흰색으로 시작하는 경우
-                        if board[y][x] != 'B':
-                            paintBCnt += 1
+        # 검은색으로 시작하는 경우
+        BCnt = 0
+        # 흰색으로 시작하는 경우
+        WCnt = 0
+        for x in range(i, i + 8):
+            for y in range(j, j + 8):
+                if (x + y) % 2 == 0:
+                    # board[i][j] = 'B'인 경우 = 검은색으로 시작하는 경우
+                    if board[x][y] != 'B':
+                        BCnt += 1
+                    # board[i][j] = 'W'인 경우 = 흰색으로 시작하는 경우
+                    if board[x][y] != 'W':
+                        WCnt += 1
                 else:
-                    if (x + y) % 2 == 0:
-                        # board[i][j] = 'B'인 경우 = 검은색으로 시작하는 경우
-                        if board[y][x] != 'W':
-                            paintWCnt += 1
-                        # board[i][j] = 'W'인 경우 = 흰색으로 시작하는 경우
-                        if board[y][x] != 'B':
-                            paintBCnt += 1
-                    else:
-                        # board[i][j] = 'B'인 경우 = 검은색으로 시작하는 경우
-                        if board[y][x] != 'B':
-                            paintBCnt += 1
-                        # board[i][j] = 'W'인 경우 = 흰색으로 시작하는 경우
-                        if board[y][x] != 'W':
-                            paintWCnt += 1
-        minCnt = min(minCnt, paintBCnt, paintWCnt)
+                    # board[i][j] = 'B'인 경우 = 검은색으로 시작하는 경우
+                    if board[x][y] != 'W':
+                        BCnt += 1
+                    # board[i][j] = 'W'인 경우 = 흰색으로 시작하는 경우
+                    if board[x][y] != 'B':
+                         WCnt += 1
+        minCnt = min(minCnt, BCnt, WCnt)
 print(minCnt)
