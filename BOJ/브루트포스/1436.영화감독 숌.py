c9ce427ffa85d5ca391ed5506e3bc7f93f1de339
comom87 <dmsrkql104321@gmail.com>
2025-01-17 00:13:15 +0900

    백준: 1436.영화감독 숌

---
+++ BOJ/브루트포스/1436.영화감독 숌.py
@@ -0,0 +1,15 @@
+# 진짜 브루트포스네...ㅎ
+# While True로 일일이 탐색해도 n < 10000이므로 반복문이 최대 100만번만 돈다.
+
+n = int(input())
+order = 1
+endNum = 666
+
+while True:
+    if order == n:
+        print(endNum)
+        break
+
+    endNum += 1
+    if '666' in str(endNum):
+        order += 1
