97184d1a4de704d272114248fd131a1988c2c9e2
comom87 <dmsrkql104321@gmail.com>
2025-01-20 02:40:41 +0900

    백준: 2217.로프

---
+++ BOJ/그리디/2217.로프.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+rope = [int(input()) for _ in range(n)]
+rope.sort(reverse=True)
+max_weight = 0
+for i in range(n):
+    max_weight = max(max_weight, rope[i] * (i + 1))
+print(max_weight)
