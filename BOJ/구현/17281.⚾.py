8bbc398d11c137742f76b2d33f19a6f7b2b01e52
comom87 <dmsrkql104321@gmail.com>
2025-01-24 21:18:07 +0900

    백준:17281.⚾️

--- BOJ/구현/17281.⚾.py
+++ BOJ/구현/17281.⚾.py
@@ -1,65 +1,41 @@
+# 구현 순서
+# 1. 타자의 순서를 정한다.
+# 2. 3아웃 전까지는 현재 이닝에 대한 득점을 계산한다.
+# 3. 3아웃이면 다음 이닝을 시작한다.
+
 import sys
+from itertools import permutations
 input = sys.stdin.readline
-
-def line_up(depth):
-    if depth == 9:
-        lineups.append(arr[:])
-        return
-
-    if depth == 3:
-        line_up(depth + 1)
-
-    for i in range(1, 9):
-        if not visited[i]:
-            visited[i] = True
-            arr[depth] = i
-            line_up(depth + 1)
-            arr[depth] = 0
-            visited[i] = False
-
 
 n = int(input())
 inning = [list(map(int, input().split())) for _ in range(n)]
-
-lineups = []
-arr = [0] * 9
-visited = [False] * 9
-line_up(0)
-
-base = [False] * 3
 max_score = 0
-for lineup in lineups:
+for batter in permutations(range(1, 9), 8):
+    batter = list(batter)
+    lineup = batter[:3] + [0] + batter[3:]
     order = 0
     score = 0
+
     for i in range(n):
+        base1, base2, base3 = 0, 0, 0
         out = 0
         while out < 3:
             if inning[i][lineup[order]] == 0:
                 out += 1
-                order = (order + 1) % 9
-                continue
-
-            for k in range(3):
-                if base[k]:
-                    if k + inning[i][lineup[order]] > 2:
-                        base[k] = False
-                        score += 1
-                    else:
-                        base[k] = False
-                        base[k + inning[i][lineup[order]]] = True
-
-            if inning[i][lineup[order]] == 4:
-                score += 1
-            else:
-                base[inning[i][lineup[order]] - 1] = True
+            elif inning[i][lineup[order]] == 1:
+                score += base3
+                base3, base2, base1 = base2, base1, 1
+            elif inning[i][lineup[order]] == 2:
+                score += base2 + base3
+                base3, base2, base1 = base1, 1, 0
+            elif inning[i][lineup[order]] == 3:
+                score += base1 + base2 + base3
+                base3, base2, base1 = 1, 0, 0
+            elif inning[i][lineup[order]] == 4:
+                score += base1 + base2 + base3 + 1
+                base3, base2, base1 = 0, 0, 0
+            
             order = (order + 1) % 9
-    
+
     max_score = max(max_score, score)
 print(max_score)
-
-
-
-
-# 1. 타자의 순서를 정한다.
-# 2. 각 이닝에 대해 점수를 계산하다가 3아웃이 발생하면 다음 이닝으로 넘어간다.
-# 3. 각 타자의 순서에서 최댓값으 구한다.
