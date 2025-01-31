a2fce99d2445e6b8500bc7f981d129f56d2505fa
comom87 <dmsrkql104321@gmail.com>
2025-01-31 19:05:07 +0900

    백준: 2293.동전1

---
+++ BOJ/다이나믹 프로그래밍/2293.동전1.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dp = [0] * (k + 1)
+dp[0] = 1
+
+# 동전의 구성은 같은데 순서만 다른 것을 제외하기 위하여 동전 하나에 대하여 연산을 진행하고 이를 갱신하는 방식으로 진행
+for _ in range(n):
+    coin = int(input())
+    for i in range(coin, k + 1):
+        # dp[i] += dp[i - 동전의 가치]
+        dp[i] += dp[i - coin]
+print(dp[k])
