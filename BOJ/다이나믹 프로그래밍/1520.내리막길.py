2f5da53ac2a7b9eafd492266e518dc74b8b2d588
comom87 <dmsrkql104321@gmail.com>
2025-02-05 20:26:51 +0900

    백준: 1520.내리막길

--- BOJ/다이나믹 프로그래밍/1520.내리막길.py
+++ BOJ/다이나믹 프로그래밍/1520.내리막길.py
@@ -33,6 +33,8 @@ dp = [[0 for _ in range(n)] for _ in range(m)]
 BFS(graph[0][0], 0, 0)
 print(dp[m - 1][n - 1])
 
+
+
 # 2
 # DFS
 import sys
@@ -67,3 +69,43 @@ graph = [list(map(int, input().split())) for _ in range(m)]
 # dp의 원소를 0으로 설정하면 내리막길로만 이동하는 경우의 수가 없는 경우와 방문 여부를 확인하는 경우를 구분할 수 없다.
 dp = [[-1 for _ in range(n)] for _ in range(m)]
 print(DFS(0, 0))
+
+
+
+# 3
+# 시간 초과: 상하좌우를 전부 확인해서 큐에 넣었다 뺐다 하는 방식으로는 500 * 500 개의 상하좌우를 확인해야 한다고 최악을 가정한다면 4 ^ (500 x 500) 연산 횟수가 진행될 것 같습니다.
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+
+# dx = [0, 1, 0, -1]
+# dy = [1, 0, -1, 0]
+
+# def BFS(x, y):
+#     global cnt
+
+#     queue = deque()
+#     queue.append((x, y))
+
+#     while queue:
+#         x, y = queue.popleft()
+
+#         if x == m - 1 and y == n - 1:
+#             cnt += 1
+#             continue
+
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+
+#             if nx < 0 or nx >= m or ny < 0 or ny >= n:
+#                 continue
+
+#             if graph[nx][ny] < graph[x][y]:
+#                 queue.append((nx, ny))
+                
+# m, n = map(int, input().split())
+# graph = [list(map(int, input().split())) for _ in range(m)]
+# cnt = 0
+# BFS(0, 0)
+# print(cnt)
