5b3039c584508c0f1acb9fbade93aee4b810b14b
comom87 <dmsrkql104321@gmail.com>
2025-02-10 22:12:30 +0900

    백준: 9657.돌 게임3

---
+++ BOJ/다이나믹 프로그래밍/9657.돌 게임3.py
@@ -0,0 +1,12 @@
+n = int(input())
+dp = [''] * 1001
+dp[1] = 'SK'
+dp[2] = 'CY'
+dp[3] = 'SK'
+dp[4] = 'SK'
+for i in range(5, n + 1):
+    if dp[i - 1] == 'SK' and dp[i - 3] == 'SK' and dp[i - 4] == 'SK':
+        dp[i] = 'CY'
+    else:
+        dp[i] = 'SK'
+print(dp[n])
