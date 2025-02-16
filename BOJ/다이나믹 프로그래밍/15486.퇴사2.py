79f3f0d58bd995dcc9a8c5be704941f90301145b
comom87 <dmsrkql104321@gmail.com>
2025-02-16 21:01:07 +0900

    백준: 15486.퇴사2

---
+++ BOJ/다이나믹 프로그래밍/15486.퇴사2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n + 1)
+
+for day in range(1, n + 1):
+    t, p = map(int, input().split())
+    dp[day] = max(dp[day], dp[day - 1])
+    if day + t - 1 <= n:
+        dp[day + t - 1] = max(dp[day + t - 1], dp[day - 1] + p)
+print(dp[n])
