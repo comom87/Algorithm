6c7835c49c74e02d4e7c173b89b9a056eb2478d1
comom87 <dmsrkql104321@gmail.com>
2025-02-18 20:05:29 +0900

    백준: 13398.연속합2

---
+++ BOJ/다이나믹 프로그래밍/13398.연속합2.py
@@ -0,0 +1,16 @@
+# 하나의 수를 제거하거나 제거하지 않는 2가지 경우를 모두 고려하는 것까지는 생각했는데...
+# 왜 갱신하는 방법을 생각을 못하니!!!
+# 11번째 줄과 관련된 반례: https://www.acmicpc.net/board/view/141372
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+dp = [[0 for _ in range(n)] for _ in range(2)]
+dp[0][0] = nums[0]
+dp[1][0] = -1000
+
+for i in range(1, n):
+    dp[0][i] = max(dp[0][i - 1] + nums[i], nums[i])
+    dp[1][i] = max(dp[0][i - 1], dp[1][i - 1] + nums[i])
+print(max(max(dp[0]), max(dp[1])))
