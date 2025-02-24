9ab583075fb810b7c17dc97077d36175142a60cb
comom87 <dmsrkql104321@gmail.com>
2025-02-25 01:39:01 +0900

    백준: 1915.가장 큰 정사각형

--- BOJ/다이나믹 프로그래밍/1915.가장 큰 정사각형.py
+++ BOJ/다이나믹 프로그래밍/1915.가장 큰 정사각형.py
@@ -6,6 +6,7 @@ matrix = [[0 for _ in range(m + 1)]]
 for _ in range(n):
     matrix.append([0] + list(map(int, input().rstrip())))
 dp = [[matrix[i][j] for j in range(m + 1)] for i in range(n + 1)]
+
 for i in range(1, n + 1):
     for j in range(1, m + 1):
         if matrix[i][j] == 1:
