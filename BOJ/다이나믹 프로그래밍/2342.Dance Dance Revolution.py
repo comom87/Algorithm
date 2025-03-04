5c990c686c621fb5dab34e6b724e40ba528b06ad
comom87 <dmsrkql104321@gmail.com>
2025-03-04 12:05:24 +0900

    백준: 2342.Dance Dance Revolution

---
+++ BOJ/다이나믹 프로그래밍/2342.Dance Dance Revolution.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+nums = list(map(int, input().split()))
+dp = [[[1e9 for _ in range(5)] for _ in range(5)] for _ in range(len(nums))]
+dp[0][0][0] = 0
+
+def calculate_cost(previous_point, new_point):
+    if previous_point == new_point:
+        return 1
+    elif previous_point == 0:
+        return 2
+    elif abs(previous_point - new_point) == 2:
+        return 4
+    else:
+        return 3
+
+
+for i in range(len(nums) - 1):
+    num = nums[i]
+    for left in range(5):
+        for right in range(5):
+            dp[i + 1][num][right] = min(dp[i + 1][num][right], dp[i][left][right] + calculate_cost(left, num))
+            dp[i + 1][left][num] = min(dp[i + 1][left][num], dp[i][left][right] + calculate_cost(right, num))
+print(min(map(min, dp[-1])))
