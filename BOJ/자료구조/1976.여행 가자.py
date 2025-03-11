c441ec37af00195ee318eb30644cfa947f41dbc5
comom87 <dmsrkql104321@gmail.com>
2025-03-11 20:13:20 +0900

    백준: 1976.여행 가자

---
+++ BOJ/자료구조/1976.여행 가자.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if x == parent[x]:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+n = int(input())
+parent = [i for i in range(n)]
+m = int(input())
+for i in range(n):
+    arr = list(map(int, input().split()))
+    for j in range(n):
+        if arr[j] == 1:
+            union(i, j)
+
+city = list(map(int, input().split()))
+can_visit = 'YES'
+for i in range(1, m):
+    if parent[city[i] - 1] != parent[city[i + 1] - 1]:
+        can_visit = 'NO'
+        break
+print(can_visit)
